acef1e5661f6edf34897e65b674c950d82e4367d
Jean Santos <jeansaantos89@gmail.com>
2025-04-30 11:33:40 -0300

    30-04

---
+++ tests/test_product_detail.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Page
+import pytest
+
+def test_loginUser(page: Page):
+    page.goto('http://automationexercise.com')
+    products_btn = page.wait_for_selector('a[href="/products"]').click() # Entre na aba produtos
+    primeiro_produto = page.wait_for_selector('a[href="/product_details/1"]').click() # Entra no primeiro produto disponível
+
+    try: # Verifica a presença das informações necessárias
+        availability = page.wait_for_selector('b:has-text("Availability")')
+        condition = page.wait_for_selector('b:has-text("Condition")')
+        brand = page.wait_for_selector('b:has-text("Brand")', timeout = 3000)
+        assert availability.is_visible()
+        assert condition.is_visible()
+        assert brand.is_visible()
+    except TimeoutError:
+        pytest.fail("Não possui as informações no produto")
